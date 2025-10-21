2fd82e3bac4d3f3afc72e7b4c5330081e5683e3b
Jefferson Gilberto Alquinga Chuquimarca <144504631+Jeffers199817@users.noreply.github.com>
2025-10-21 10:26:29 -0500

    Update

--- src/main/java/com/pichincha/spfmsaapexcoreservice/configuration/CorsConfig.java
+++ src/main/java/com/pichincha/spfmsaapexcoreservice/configuration/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.pichincha.spfmsaapexcoreservice.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,15 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    
+    @Value("${cors.allowed.origins:http://localhost:4200,http://vps-5405471-x.dattaweb.com:4200}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins(allowedOrigins.split(","))
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*");
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
             }
         };
     }
